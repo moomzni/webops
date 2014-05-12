7248e78c7eadfbed9628c23c69d334e731f54388
Caoimhin Graham <caoimhin.graham@digital.cabinet-office.gov.uk>
2014-05-12 15:44:53 +0100

    custom fact updates

--- day_1/6.custom_facts/puppet/manifests/site.pp
+++ day_1/6.custom_facts/puppet/manifests/site.pp
@@ -1,14 +1,16 @@
 node default {
 
+  package { 'vim' : ensure => latest, }
+
   case $::hostname {
     node1   : { include nginx }
     node2   : { include apache }
     default : { }
   }
 
-  # case $::kainos_database_type {
-  #   mongodb:        { package { 'mongodb' : ensure => 'latest', } }
-  #   /^postgresql$/: { package { 'postgresql-9.1' : ensure => 'latest', } }
-  #   default:        { }
-  # }
+  case $::kainos_database_type {
+    mongodb:    { package { 'mongodb' : ensure => 'latest', } }
+    postgresql: { package { 'postgresql-9.1' : ensure => 'latest', } }
+    default:    { }
+  }
 }
