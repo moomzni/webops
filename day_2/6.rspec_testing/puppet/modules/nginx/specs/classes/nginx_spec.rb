f83abbc416e6d034301b66539b3bc004e07f5091
Caoimhin Graham <caoimhin.graham@digital.cabinet-office.gov.uk>
2014-05-13 22:57:11 +0100

    adding additional spec tests to example

--- day_2/6.rspec_testing/puppet/modules/nginx/specs/classes/nginx_spec.rb
+++ day_2/6.rspec_testing/puppet/modules/nginx/specs/classes/nginx_spec.rb
@@ -2,9 +2,9 @@ require_relative '../../../../spec_helper'
 
 describe 'nginx', :type => :class do
   it do
-    should contain_package('nginx').with_ensure('latest')
-    should contain_file('/etc/nginx/nginx.conf')
+    should contain_package('nginx').with_ensure('latest').with_provider('apt')
 
+    should contain_file('/etc/nginx/nginx.conf')
 
     should contain_file('/etc/nginx/sites-available', '/etc/nginx/sites-enabled').with_ensure('directory')
 
