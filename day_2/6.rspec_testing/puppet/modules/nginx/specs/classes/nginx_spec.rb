9130f08edfa6407971b16584da5f58cf4d7d8542
Caoimhin Graham <caoimhin.graham@digital.cabinet-office.gov.uk>
2014-05-13 14:36:21 +0100

    additional rspec tests

--- day_2/6.rspec_testing/puppet/modules/nginx/specs/classes/nginx_spec.rb
+++ day_2/6.rspec_testing/puppet/modules/nginx/specs/classes/nginx_spec.rb
@@ -1,10 +1,13 @@
 require_relative '../../../../spec_helper'
 
 describe 'nginx', :type => :class do
-
   it do
     should contain_package('nginx').with_ensure('latest')
     should contain_file('/etc/nginx/nginx.conf')
+
+
+    should contain_file('/etc/nginx/sites-available', '/etc/nginx/sites-enabled').with_ensure('directory')
+
     should contain_service('nginx').with_ensure('running')
   end
 end
--- day_2/6.rspec_testing/puppet/modules/nginx/specs/defines/vhost_spec.rb
+++ day_2/6.rspec_testing/puppet/modules/nginx/specs/defines/vhost_spec.rb
@@ -25,3 +25,29 @@ describe 'nginx::vhost', :type => :define do
       with_target('/etc/nginx/sites-available/test-website')
   end
 end
+
+describe 'nginx::vhost', :type => :define do
+
+  let(:title) { 'caoimhin' }
+  let(:params) {
+    {
+      'port'       => '100',
+      'content'    => 'bob'
+    }
+  }
+  it do
+    should contain_file('/etc/nginx/sites-available/caoimhin').
+      with_ensure('present').
+      with_owner('root').
+      with_group('root').
+      with_mode('0644').
+      with_content(/bob/).
+      with_content(/listen 100;/).
+      with_content(/access_log \/var\/log\/nginx\/caoimhin-access\.log;/).
+      with_content(/error_log \/var\/log\/nginx\/caoimhin-error\.log;/)
+
+    should contain_file('/etc/nginx/sites-enabled/caoimhin').
+      with_ensure('link').
+      with_target('/etc/nginx/sites-available/caoimhin')
+  end
+end
